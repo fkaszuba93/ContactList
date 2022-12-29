f5a1aff81d98907ad4e3b069d229c5daa5b77895
Filip Kaszuba <filip.kaszuba@gmail.com>
2022-12-29 17:46:26 +0100

    create project

---
+++ ContactList/ContactListProject/Form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactListProject
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
