03d27600920a18c7e658ed3595850d162edadc6b
Filip Kaszuba <fkaszuba93@gmail.com>
2023-01-02 13:33:51 +0100

    create Contacts class

---
+++ ContactList/ContactListProject/model/Contacts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ContactListProject.model
+{
+    public class Contacts
+    {
+        private List<Contact> contactList;
+
+        public Contacts()
+        {
+            contactList = new List<Contact>();
+        }
+
+        public static Contacts ReadFromFile(string path)
+        {
+            Contacts contacts = new Contacts();
+
+            var data = File.ReadLines(path);
+            foreach (string line in data)
+            {
+                contacts.contactList.Add(Contact.Parse(line));
+            }
+            return contacts;
+        }
+    }
+}
